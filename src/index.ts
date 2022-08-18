44af972dd1a919e5ea5f7f6d354167b0d7ff1748
bobolife <bbw1996@qq.com>
2022-08-19 01:29:57 +0800

    chore: 支持jsx语法

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import SButton from './button'
+// import SButton from './button'
+import SFCButton from './SFCButton.vue'
 
-const app = createApp(SButton)
+const app = createApp(SFCButton)
 app.mount('#app')
